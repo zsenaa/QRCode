4ac9fd55be8a60e128a77bc2724b2bcb624e9aa5
Sena <zhrsnkunt@gmail.com>
2023-11-18 19:30:26 +0300

    Phone Vcard QR Code
    A personalized QR code is created by entering the name, surname, phone number and email information. When we scan the created QR code on our phone, the add to phone book section opens. If we want, we can easily save that person to our phone book.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace PhoneQRCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneLabel2 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneLabel3 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneLabel4 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneTextBox1 = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.siticoneTextBox2 = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.siticoneTextBox3 = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.siticoneTextBox4 = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.siticoneButton1);
+            this.groupBox1.Controls.Add(this.siticoneTextBox4);
+            this.groupBox1.Controls.Add(this.siticoneTextBox3);
+            this.groupBox1.Controls.Add(this.siticoneTextBox2);
+            this.groupBox1.Controls.Add(this.siticoneTextBox1);
+            this.groupBox1.Controls.Add(this.siticoneLabel4);
+            this.groupBox1.Controls.Add(this.siticoneLabel3);
+            this.groupBox1.Controls.Add(this.siticoneLabel2);
+            this.groupBox1.Controls.Add(this.siticoneLabel1);
+            this.groupBox1.Font = new System.Drawing.Font("MS PGothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(570, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "VCARD";
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Location = new System.Drawing.Point(22, 31);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(104, 35);
+            this.siticoneLabel1.TabIndex = 0;
+            this.siticoneLabel1.Text = "Name :";
+            // 
+            // siticoneLabel2
+            // 
+            this.siticoneLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel2.Location = new System.Drawing.Point(22, 96);
+            this.siticoneLabel2.Name = "siticoneLabel2";
+            this.siticoneLabel2.Size = new System.Drawing.Size(151, 35);
+            this.siticoneLabel2.TabIndex = 1;
+            this.siticoneLabel2.Text = "Surname :";
+            // 
+            // siticoneLabel3
+            // 
+            this.siticoneLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel3.Location = new System.Drawing.Point(22, 160);
+            this.siticoneLabel3.Name = "siticoneLabel3";
+            this.siticoneLabel3.Size = new System.Drawing.Size(175, 35);
+            this.siticoneLabel3.TabIndex = 2;
+            this.siticoneLabel3.Text = "Telephone :";
+            // 
+            // siticoneLabel4
+            // 
+            this.siticoneLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel4.Location = new System.Drawing.Point(22, 216);
+            this.siticoneLabel4.Name = "siticoneLabel4";
+            this.siticoneLabel4.Size = new System.Drawing.Size(118, 35);
+            this.siticoneLabel4.TabIndex = 3;
+            this.siticoneLabel4.Text = "E-mail :";
+            // 
+            // siticoneTextBox1
+            // 
+            this.siticoneTextBox1.BorderRadius = 5;
+            this.siticoneTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTextBox1.DefaultText = "";
+            this.siticoneTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox1.DisabledState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox1.FocusedState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox1.HoveredState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Location = new System.Drawing.Point(250, 31);
+            this.siticoneTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.siticoneTextBox1.Name = "siticoneTextBox1";
+            this.siticoneTextBox1.PasswordChar = '\0';
+            this.siticoneTextBox1.PlaceholderText = "";
+            this.siticoneTextBox1.SelectedText = "";
+            this.siticoneTextBox1.ShadowDecoration.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Size = new System.Drawing.Size(242, 36);
+            this.siticoneTextBox1.TabIndex = 4;
+            // 
+            // siticoneTextBox2
+            // 
+            this.siticoneTextBox2.BorderRadius = 5;
+            this.siticoneTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTextBox2.DefaultText = "";
+            this.siticoneTextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneTextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneTextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox2.DisabledState.Parent = this.siticoneTextBox2;
+            this.siticoneTextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox2.FocusedState.Parent = this.siticoneTextBox2;
+            this.siticoneTextBox2.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox2.HoveredState.Parent = this.siticoneTextBox2;
+            this.siticoneTextBox2.Location = new System.Drawing.Point(250, 96);
+            this.siticoneTextBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.siticoneTextBox2.Name = "siticoneTextBox2";
+            this.siticoneTextBox2.PasswordChar = '\0';
+            this.siticoneTextBox2.PlaceholderText = "";
+            this.siticoneTextBox2.SelectedText = "";
+            this.siticoneTextBox2.ShadowDecoration.Parent = this.siticoneTextBox2;
+            this.siticoneTextBox2.Size = new System.Drawing.Size(242, 36);
+            this.siticoneTextBox2.TabIndex = 5;
+            // 
+            // siticoneTextBox3
+            // 
+            this.siticoneTextBox3.BorderRadius = 5;
+            this.siticoneTextBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTextBox3.DefaultText = "";
+            this.siticoneTextBox3.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneTextBox3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneTextBox3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox3.DisabledState.Parent = this.siticoneTextBox3;
+            this.siticoneTextBox3.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox3.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox3.FocusedState.Parent = this.siticoneTextBox3;
+            this.siticoneTextBox3.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox3.HoveredState.Parent = this.siticoneTextBox3;
+            this.siticoneTextBox3.Location = new System.Drawing.Point(250, 160);
+            this.siticoneTextBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.siticoneTextBox3.Name = "siticoneTextBox3";
+            this.siticoneTextBox3.PasswordChar = '\0';
+            this.siticoneTextBox3.PlaceholderText = "";
+            this.siticoneTextBox3.SelectedText = "";
+            this.siticoneTextBox3.ShadowDecoration.Parent = this.siticoneTextBox3;
+            this.siticoneTextBox3.Size = new System.Drawing.Size(242, 36);
+            this.siticoneTextBox3.TabIndex = 6;
+            this.siticoneTextBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.siticoneTextBox3_KeyPress);
+            // 
+            // siticoneTextBox4
+            // 
+            this.siticoneTextBox4.BorderRadius = 5;
+            this.siticoneTextBox4.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTextBox4.DefaultText = "";
+            this.siticoneTextBox4.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneTextBox4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneTextBox4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox4.DisabledState.Parent = this.siticoneTextBox4;
+            this.siticoneTextBox4.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox4.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox4.FocusedState.Parent = this.siticoneTextBox4;
+            this.siticoneTextBox4.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox4.HoveredState.Parent = this.siticoneTextBox4;
+            this.siticoneTextBox4.Location = new System.Drawing.Point(250, 215);
+            this.siticoneTextBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.siticoneTextBox4.Name = "siticoneTextBox4";
+            this.siticoneTextBox4.PasswordChar = '\0';
+            this.siticoneTextBox4.PlaceholderText = "";
+            this.siticoneTextBox4.SelectedText = "";
+            this.siticoneTextBox4.ShadowDecoration.Parent = this.siticoneTextBox4;
+            this.siticoneTextBox4.Size = new System.Drawing.Size(242, 36);
+            this.siticoneTextBox4.TabIndex = 7;
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.FillColor = System.Drawing.Color.DarkOrchid;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(283, 279);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton1.TabIndex = 8;
+            this.siticoneButton1.Text = "Generate QR";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 258);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(41)))), ((int)(((byte)(214)))));
+            this.ClientSize = new System.Drawing.Size(594, 450);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel4;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel3;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel2;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.UI.WinForms.SiticoneTextBox siticoneTextBox4;
+        private Siticone.UI.WinForms.SiticoneTextBox siticoneTextBox3;
+        private Siticone.UI.WinForms.SiticoneTextBox siticoneTextBox2;
+        private Siticone.UI.WinForms.SiticoneTextBox siticoneTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneQRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneTextBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            string ad = siticoneTextBox1.Text;
+            string soyad = siticoneTextBox2.Text;
+            string telefon = siticoneTextBox3.Text;
+            string email = siticoneTextBox4.Text;
+
+            string vcardText = $"BEGIN:VCARD\nVERSION:3.0\nN:{soyad};{ad}\nFN:{ad} {soyad}\nTEL:{telefon}\nEMAIL:{email}\nEND:VCARD";
+
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(vcardText, QRCodeGenerator.ECCLevel.Q);
+            QRCodeData qrCodeData1 = qrGenerator.CreateQrCode(vcardText, QRCodeGenerator.ECCLevel.Q);
+            QRCodeData qrCodeData2 = qrGenerator.CreateQrCode(vcardText, QRCodeGenerator.ECCLevel.Q);
+            QRCodeData qrCodeData3 = qrGenerator.CreateQrCode(vcardText, QRCodeGenerator.ECCLevel.Q);
+
+
+            QRCode qrCode = new QRCode(qrCodeData);
+            QRCode qrCode1 = new QRCode(qrCodeData1);
+            QRCode qrCode2 = new QRCode(qrCodeData2);
+            QRCode qrCode3 = new QRCode(qrCodeData3);
+
+            Bitmap qrCodeImage = qrCode.GetGraphic(7);
+            Bitmap qrCodeImage1 = qrCode1.GetGraphic(7);
+            Bitmap qrCodeImage2 = qrCode2.GetGraphic(7);
+            Bitmap qrCodeImage3 = qrCode3.GetGraphic(7);
+            pictureBox1.Image = qrCodeImage;
+            pictureBox1.Image = qrCodeImage1;
+            pictureBox1.Image = qrCodeImage2;
+            pictureBox1.Image = qrCodeImage3;
+
+
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = @"PNG|*.png" })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    pictureBox1.Image.Save(saveFileDialog.FileName);
+                    MessageBox.Show("the file is saved");
+                }
+            }
+            siticoneTextBox1.Text = "";
+            siticoneTextBox2.Text = "";
+            siticoneTextBox3.Text = "";
+            siticoneTextBox4.Text = "";
+            pictureBox1.Image = null;
+        }
+    }
+    }
+
